08066184809edb026d1e4a248590d846a9c8e0d1
Bal-tech <limpar10@gmail.com>
2023-06-29 22:08:01 -0700

    second commit

--- counterIncrement.js
+++ counterIncrement.js
@@ -1,19 +1,27 @@
 // useState hook in React
-const {useState} = React;
-// import React, { useState } from 'react';
-// import ReactDOM from 'react-dom';
+// const {useState} = React;
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
+// functional component
 const CounterButton = () => {
-  const [count, setCount] = useState(27);
+    console.log('inside counter button')
+    // [ access, set] = useState(original value/data);
+  const [count, setCount] = useState(27); // initial value is 27
 
   const incrementCount = () => {
+    console.log("clicked");
     setCount(count + 1);
   };
 
-  return (
+  return (  
     <div>
-      <h1>Counter: {count}</h1>
-      <button onClick={incrementCount}> Increment</button>
+      <h1>Counter: {count} </h1>
+      <button onClick = {incrementCount} > Increment </button>
     </div>
   );
+
 };
+
+let root = ReactDOM.createRoot(document.getElementById('mydiv'));
+root.render(< CounterButton/>);
